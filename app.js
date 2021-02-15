4a9dffc3f6dc1ddf942e5fc4ad3f91bbe40944c7
TON <poolsak.kru@gmail.com>
2021-02-16 01:44:38 +0700

    end course

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const app = Vue.createApp({
     data() {
         return {
             showBooks: true,
-            title: 'The fungus',
-            author: 'Poolsak.kru',
-            age: 100
+            books: [
+                { title: 'name of the wind', author: 'patrick rothfuss' },
+                { title: 'the way of kings', author: 'brandon sanderson' },
+                { title: 'the final empire', author: 'brandon sanderson' }
+            ]
         }
     },
     methods: {
