f094e20b8ffcc4606bf69342f3875221c9ea4398
TON <poolsak.kru@gmail.com>
2021-02-16 01:32:06 +0700

    end course

--- app.js
+++ app.js
@@ -4,12 +4,24 @@ const app = Vue.createApp({
             showBooks: true,
             title: 'The fungus',
             author: 'Poolsak.kru',
-            age: 100
+            age: 100,
+            x: 0,
+            y: 0
         }
     },
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks // using reverse the exclamation !
+        },
+        handleEvent(e, data) {
+            console.log(e, e.type);
+            if(data) {
+                console.log(data);
+            }
+        },
+        handleMousemove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
 })
