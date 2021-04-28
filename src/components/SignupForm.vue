d37dfe520df191af4b9af1ad762e9fed3744e537
TON <poolsak.kru@gmail.com>
2021-04-28 23:08:17 +0700

    [44] submitting the form

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,10 +1,11 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
       <label>Email:</label>
       <input type="email" required v-model="email">
 
       <label>Password:</label>
       <input type="password" required v-model="password">
+      <div v-if="passwordError" class="error">{{ passwordError }}</div>
 
       <label>Role:</label>
       <select v-model="role">
@@ -23,6 +24,10 @@
         <label>Accept terms and conditions</label>
       </div>
 
+      <div class="submit">
+          <button>Create an Account</button>
+      </div>
+
   </form>
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
@@ -39,7 +44,8 @@ export default {
             role: 'developer',
             terms: false,
             tempSkill: '',
-            skills: []
+            skills: [],
+            passwordError: ''
         }
     },
     methods: {
@@ -55,6 +61,19 @@ export default {
             this.skills = this.skills.filter((item) => {
                 return  skill !== item
             })
+        },
+        handleSubmit() {
+            // validate password
+            this.passwordError = this.password.length > 5 ? 
+                '' : 'Password must be at least 6 chars long'
+
+            if(!this.passwordError) {
+                 console.log('email', this.email);
+                 console.log('password', this.password);
+                 console.log('role', this.role);
+                 console.log('skills', this.skills);
+                 console.log('terms accepted', this.terms);
+            }
         }
     }
 }
@@ -109,5 +128,25 @@ input[type="checkbox"] {
     font-weight: bold;
     color: #777;
     cursor: pointer;
+}
+
+button {
+    background: #0b6dff;
+    border: 0;
+    padding: 10px 20px;
+    margin-top: 20px;
+    color: white;
+    border-radius: 20px;
+}
+
+.submit {
+    text-align: center;
+}
+
+.error {
+    color: #ff0062;
+    margin-top: 10px;
+    font-size: 0.8em;
+    font-weight: bold;
 }
 </style>
