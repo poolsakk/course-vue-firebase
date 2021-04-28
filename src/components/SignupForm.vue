9d24a04dc46dd3326b39b3fd6af2542852bf4423
TON <poolsak.kru@gmail.com>
2021-04-28 21:42:18 +0700

    [40] select fields

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -5,9 +5,17 @@
 
       <label>Password:</label>
       <input type="password" required v-model="password">
+
+      <label>Role:</label>
+      <select v-model="role">
+          <option value="developer">Web Developer</option>
+          <option value="designer">Web Designer</option>
+      </select>
+
   </form>
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
+  <p>Role: {{ role }}</p>
 </template>
 
 <script>
@@ -15,7 +23,8 @@ export default {
     data() {
         return {
             email: '',
-            password: ''
+            password: '',
+            role: 'developer'
         }
     }
 }
@@ -41,7 +50,7 @@ label {
     font-weight: bold;
 }
 
-input {
+input, select {
     display: block;
     padding: 10px 6px;
     width: 100%;
