7a80baeeace56843fa2f643464a350d3c9830cce
TON <poolsak.kru@gmail.com>
2021-04-29 03:28:57 +0700

    [61] deleting projects

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -4,7 +4,7 @@
           <h3 @click="showDetails = !showDetails">{{ project.title }}</h3>
           <div class="icons">
             <span class="material-icons">edit</span>
-            <span class="material-icons">delete</span>
+            <span @click="deleteProject" class="material-icons">delete</span>
             <span class="material-icons">done</span>
           </div>
       </div>
@@ -19,7 +19,15 @@ export default {
     props: ['project'],
     data() {
         return {
-            showDetails: false
+            showDetails: false,
+            uri: 'http://localhost:3000/projects/' + this.project.id
+        }
+    },
+    methods: {
+        deleteProject() {
+            fetch(this.uri, { method: 'DELETE' })
+                .then(() => this.$emit('delete', this.project.id))
+                .catch(err => console.log(err.message))
         }
     }
 }
