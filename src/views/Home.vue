be58c2447b3654d1438edf17edd5fab6268e200f
TON <poolsak.kru@gmail.com>
2021-04-29 17:58:40 +0700

    [76] using watch and watch effect

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,12 +4,13 @@
     <input type="text" v-model="search">
     <p>Search term - {{ search }}</p>
     <div v-for="name in matchingNames" :key="name">{{ name }}</div>
+    <button @click="handleClick">stop watching</button>
   </div>
 </template>
 
 <script>
 import { ref } from '@vue/reactivity'
-import { computed } from '@vue/runtime-core'
+import { computed, watch, watchEffect } from '@vue/runtime-core'
 
 export default {
   name: 'Home',
@@ -17,11 +18,24 @@ export default {
     const search = ref('')
     const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
 
+    const stopWatch = watch(search, () => {
+      console.log('watch function ran');
+    })
+
+    const stopEffect = watchEffect(() => {
+      console.log('watch effect function ran', search.value);
+    })
+
     const matchingNames = computed(() => {
       return names.value.filter((name) => name.includes(search.value))
     })
 
-    return { names, search, matchingNames }
+    const handleClick = () => {
+      stopWatch()
+      stopEffect()
+    }
+
+    return { names, search, matchingNames, handleClick }
   }
 }
 </script>
