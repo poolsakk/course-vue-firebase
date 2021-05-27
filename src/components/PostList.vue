d246c74be522e741181fde9e97bfe3ed7f492e74
TON <poolsak.kru@gmail.com>
2021-05-27 18:17:31 +0700

    [78] lifecycle hooks

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -8,14 +8,17 @@
 </template>
 
 <script>
+import { onMounted, onUnmounted, onUpdated } from '@vue/runtime-core'
 import SinglePost from '../components/SinglePost.vue'
 
 export default {
     props: ['posts'],
     components: { SinglePost },
     setup(props) {
-        console.log(props.posts);
-    }
+        onMounted(() => console.log('component mounted'))
+        onUnmounted(() => console.log('component unmouted'))
+        onUpdated(() => console.log('component updated'))
+    },
 }
 </script>
 
