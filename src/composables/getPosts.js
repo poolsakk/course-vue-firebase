379b54e973c7c1aba43a209194e8d9f30e77d604
TON <poolsak.kru@gmail.com>
2021-05-29 17:29:41 +0700

    [94] fetching a collection of documents

--- src/composables/getPosts.js
+++ src/composables/getPosts.js
@@ -1,4 +1,5 @@
 import { ref } from '@vue/reactivity'
+import { projectFirestore } from '../firebase/config'
 
 const getPosts = () => {
     const posts = ref([])
@@ -6,16 +7,13 @@ const getPosts = () => {
 
     const load = async () => {
       try {
-        // simulate delay
-        await new Promise(resolve => {
-            setTimeout(resolve, 2000)
+        const res = await projectFirestore.collection('posts').get()
+
+        posts.value = res.docs.map((doc) => {
+          // console.log(doc.data());
+          return { ...doc.data(), id: doc.id }
         })
 
-        let data = await fetch('http://localhost:3000/posts')
-        if(!data.ok) {
-          throw Error('no data avaliable')
-        }
-        posts.value = await data.json()
       } catch (err) {
         error.value = err.message
         console.log(error.value);
