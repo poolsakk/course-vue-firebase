4ac240c3cdf60f498e9083dd4159340fa4471b97
TON <poolsak.kru@gmail.com>
2021-06-01 16:03:15 +0700

    [95] fetching single documents

--- src/composables/getPost.js
+++ src/composables/getPost.js
@@ -1,4 +1,5 @@
 import { ref } from '@vue/reactivity'
+import { projectFirestore } from '../firebase/config'
 
 const getPost = (id) => {
     const post = ref(null)
@@ -6,16 +7,14 @@ const getPost = (id) => {
 
     const load = async () => {
       try {
-        // simulate delay
-        await new Promise(resolve => {
-            setTimeout(resolve, 2000)
-        })
-        
-        let data = await fetch('http://localhost:3000/posts/' + id)
-        if(!data.ok) {
-          throw Error('that post does not exists')
+        let res = await projectFirestore.collection('posts').doc(id).get()
+
+        if(!res.exists) {
+          throw Error('That postt does not exist')
         }
-        post.value = await data.json()
+
+        post.value = { ...res.data(), id: res.id }
+        console.log(post.value);
       } catch (err) {
         error.value = err.message
         console.log(error.value);
