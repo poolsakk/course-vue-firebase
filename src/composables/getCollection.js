6beb63a05428cf123cb57e32643cbb50360a7d4b
TON <poolsak.kru@gmail.com>
2021-06-03 02:01:02 +0700

    [122] unsubscribing from real-time listeners

--- src/composables/getCollection.js
+++ src/composables/getCollection.js
@@ -1,4 +1,4 @@
-import { ref } from 'vue'
+import { ref, watchEffect } from 'vue'
 import { projectFirestore } from '../firebase/config'
 
 const getCollection = (collection) => {
@@ -8,7 +8,8 @@ const getCollection = (collection) => {
     let collectionRef = projectFirestore.collection(collection)
         .orderBy('createdAt')
 
-    collectionRef.onSnapshot((snap) => {
+    const unsub = collectionRef.onSnapshot((snap) => {
+        console.log('snapshot');
         let results = []
         snap.docs.forEach((doc) => {
             doc.data().createdAt && results.push({ ...doc.data(), id: doc.id })
@@ -18,6 +19,10 @@ const getCollection = (collection) => {
     }, (err) => {
         documents.value = null
         error.value = 'could not fetch data'
+    })
+
+    watchEffect((onInvalidate) => {
+        onInvalidate(() => unsub())
     })
 
     return { documents, error }
