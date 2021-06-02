56a6c8ee6bc0e00a3b754ae6e3719104a9bab7d3
TON <poolsak.kru@gmail.com>
2021-06-02 20:43:20 +0700

    [116] new chat room

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <Navbar/>
+    <NewChatForm />
   </div>
   <p>Chatroom</p>
 </template>
@@ -10,9 +11,10 @@ import Navbar from '../components/Navbar.vue'
 import getUser from '../composables/getUser'
 import { watch } from 'vue'
 import { useRouter } from 'vue-router'
+import NewChatForm from '../components/NewChatForm.vue'
 
 export default {
-  components: { Navbar },
+  components: { Navbar, NewChatForm },
   setup() {
     const { user } = getUser()
     const router = useRouter()
