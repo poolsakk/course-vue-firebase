dd6c921b49ead402c0c9e030bf97b2b2e4f24f73
TON <poolsak.kru@gmail.com>
2021-06-02 16:41:45 +0700

    [103] creating a signup & login form

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+      <input type="text" required placeholder="display name" v-model="displayName">
+      <input type="text" required placeholder="email" v-model="email">
+      <input type="password" required placeholder="password" v-model="password">
+      <button>Sign up</button>
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+    setup() {
+        // refs
+        const displayName = ref('')
+        const email = ref('')
+        const password = ref('')
+
+        const handleSubmit = () => {
+            
+        }
+
+        return { displayName, email, password, handleSubmit }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="welcome container">
       <p>Welcome</p>
+      <!-- <SignupForm/> -->
+      <LoginForm/>
   </div>
 </template>
 
 <script>
+import SignupForm from '../components/SignupForm.vue'
+import LoginForm from '../components/LoginForm.vue'
+
 export default {
- 
+    components: { SignupForm, LoginForm }
 }
 </script>
 
@@ -14,5 +19,22 @@ export default {
 .welcome {
     text-align: center;
     padding: 20px 0;
+}
+.welcome form {
+    width: 300px;
+    margin: 20px auto;
+}
+.welcome label {
+    display: block;
+    margin: 20px 0 10px;
+}
+.welcome input {
+    width: 100%;
+    padding: 10px;
+    border-radius: 20px;
+    border: 1px solid #eee;
+    outline: none;
+    color: #999;
+    margin: 10px auto;
 }
 </style>
