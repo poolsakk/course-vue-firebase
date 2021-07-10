155cdd44aee38dfc59f867a7c1fa17f572d4644e
TON <poolsak.kru@gmail.com>
2021-07-11 03:57:13 +0700

    [158] deleting a playlist (composable)

---
+++ src/composables/useDocument.js
@@ -0,0 +1,27 @@
+import { ref } from 'vue'
+import { projectFirestore } from '../firebase/config'
+
+const useDocument = (collection, id) => {
+    const error = ref(null)
+    const isPending = ref(false)
+
+    let docRef = projectFirestore.collection(collection).doc(id)
+
+    const deleteDoc = async () => {
+        isPending.value = true
+        error.value = null
+        try {
+            const res = await docRef.delete()
+    
+            return res
+        } catch (err) {
+            console.log(err.message);
+            error.value = 'could not delete the document'
+            isPending.value = false
+        }
+    }
+
+    return { error, isPending, deleteDoc }
+}
+
+export default useDocument
