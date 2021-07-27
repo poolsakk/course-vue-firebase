c601cea3b1d648f12bdb3fb6d7fc451a77605cd6
TON <poolsak.kru@gmail.com>
2021-07-27 20:29:05 +0700

    [169] final touches

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,6 +7,7 @@
               <div v-if="user">
                   <router-link :to="{ name: 'CreatePlaylist' }">Create Playlist</router-link>
                   <router-link :to="{ name: 'UserPlaylists' }">My Playlist</router-link>
+                  <span>Hi here, {{ user.displayName }}</span>
                 <button @click="handleClick">Logout</button>
               </div>
               <div v-else>
@@ -68,5 +69,13 @@ nav .links a, button {
 }
 nav img {
     max-height: 60px;
+}
+
+span {
+    font-size: 14px;
+    display: inline-block;
+    margin-left: 16px;
+    padding-left: 16px;
+    border-left: 1px solid #eee;
 }
 </style>
